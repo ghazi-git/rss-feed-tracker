dec51e2ae3e8721e6870951eae3f37a36c3c74db
ghazi <ghaziabbassi1@gmail.com>
2025-08-20 22:28:09 +0100

    add svg icons as components

---
+++ src/popup/components/svg-icons/RssIcon.jsx
@@ -0,0 +1,18 @@
+export default function RssIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="3"
+      stroke="currentColor"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M12.75 19.5v-.75a7.5 7.5 0 0 0-7.5-7.5H4.5m0-6.75h.75c7.87 0 14.25 6.38 14.25 14.25v.75"
+      />
+      <circle cx="5.25" cy="18.75" r="0.75" />
+    </svg>
+  );
+}
