b459b56f2941582828102e834b9abca62c2b98b9
ghazi <ghaziabbassi1@gmail.com>
2025-08-29 14:07:22 +0100

    add a component to allow users to view all or only unread posts

---
+++ src/popup/pages/node/PostsFilter.jsx
@@ -0,0 +1,38 @@
+import { Show } from "solid-js";
+
+import Anchor from "@/popup/components/Anchor.jsx";
+import UnreadCount from "@/popup/pages/node/UnreadCount.jsx";
+
+import styles from "./PostsFilter.module.css";
+
+export default function PostsFilter(props) {
+  return (
+    <div class={styles["filter-options"]}>
+      <Anchor
+        class={`${styles.filter} ${styles.unread}`}
+        href={`/home/nodes/${props.nodeId}/posts?unread=true`}
+        activeClass={styles.active}
+        end={true}
+      >
+        <span>Unread</span>
+        <Show when={props.unreadCount}>
+          <UnreadCount
+            count={props.unreadCount}
+            onClick={(event) => {
+              event.preventDefault();
+              console.log("marked as read");
+            }}
+          />
+        </Show>
+      </Anchor>
+      <Anchor
+        class={`${styles.filter} ${styles.all}`}
+        href={`/home/nodes/${props.nodeId}/posts`}
+        activeClass={styles.active}
+        end={true}
+      >
+        <span>All</span>
+      </Anchor>
+    </div>
+  );
+}
