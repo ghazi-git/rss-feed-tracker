cf459963d789714f47db62977e430ab2b9415265
ghazi <ghaziabbassi1@gmail.com>
2025-08-29 14:31:31 +0100

    add stylelint plugin to avoid mistakenly using width/height/left/right instead of the logical alternative

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,4 +1,9 @@
-/** @type {import('stylelint').Config} */
+/** @type {import("stylelint").Config} */
 export default {
   extends: ["stylelint-config-standard"],
+  plugins: ["stylelint-plugin-logical-css"],
+  rules: {
+    "plugin/use-logical-properties-and-values": [true, { severity: "warning" }],
+    "plugin/use-logical-units": [true, { severity: "warning" }],
+  },
 };
