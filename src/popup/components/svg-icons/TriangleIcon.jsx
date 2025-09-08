a5efa7c972aab8c066cf9cc060b33dedde6a6247
ghazi <ghaziabbassi1@gmail.com>
2025-09-08 17:11:47 +0100

    replace the triangle icon with one that has rounded corners

--- src/popup/components/svg-icons/TriangleIcon.jsx
+++ src/popup/components/svg-icons/TriangleIcon.jsx
@@ -9,13 +9,12 @@ export default function TriangleIcon(props) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 12 9"
+      viewBox="0 0 220 183"
       fill="currentColor"
-      stroke="currentColor"
       class={`${styles.triangle} ${orientation() === "up" ? styles.up : ""} ${extra.class ?? ""}`}
       {...rest}
     >
-      <polygon points="1 0, 11 0, 6 8" />
+      <path d="M136.753 168.234C124.116 187.491 95.8844 187.491 83.2467 168.234L5.69093 50.0574C-8.27459 28.7772 6.99068 0.5 32.4442 0.5L187.556 0.5C213.009 0.5 228.275 28.7772 214.309 50.0574L136.753 168.234Z" />
     </svg>
   );
 }
