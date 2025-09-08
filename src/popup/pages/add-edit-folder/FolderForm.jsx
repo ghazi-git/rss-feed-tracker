2eac0e2326d91f68eb7f346302ec43362a886d85
ghazi <ghaziabbassi1@gmail.com>
2025-09-08 16:31:56 +0100

    order folder in the parent select field according to their tree hierarchy

--- src/popup/pages/add-edit-folder/FolderForm.jsx
+++ src/popup/pages/add-edit-folder/FolderForm.jsx
@@ -34,11 +34,33 @@ export default function FolderForm(props) {
 }
 
 export function getParentOptions() {
-  return NODES.filter((n) => n.type === "folder").map((n) => {
-    if (n.parentId) {
-      return { label: n.name, value: n.id };
-    } else {
-      return { label: `${n.name} (Top-level Folder)`, value: n.id };
-    }
+  const folders = NODES.filter((n) => n.type === "folder");
+  const root = folders.find((f) => f.parentId === null);
+  const orderedFolders = getFoldersTree(root, folders);
+  return orderedFolders.map(([n, level]) => {
+    return {
+      // \xa0 is a non-breaking space used to illustrate the tree hierarchy
+      label: `${"\xa0".repeat(level * 4)}${n.name}`,
+      value: n.id,
+    };
   });
+}
+
+/**
+ * return an array of folders ordered according to a DFS traversal and their
+ * sortOrder within their parent
+ */
+function getFoldersTree(rootFolder, folders) {
+  const result = [];
+  const stack = [[rootFolder, 0]];
+
+  while (stack.length > 0) {
+    const [folder, level] = stack.shift();
+    result.push([folder, level]);
+
+    const children = folders.filter((f) => f.parentId === folder.id);
+    children.sort((f1, f2) => f1.sortOrder - f2.sortOrder);
+    stack.unshift(...children.map((f) => [f, level + 1]));
+  }
+  return result;
 }
