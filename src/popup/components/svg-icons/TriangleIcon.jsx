cc091cebf4deb8361589d6f137fa01425d82a2d3
ghazi <ghaziabbassi1@gmail.com>
2025-09-09 16:37:58 +0100

    use a triangle with rounder edges as the menu state indicator

--- src/popup/components/svg-icons/TriangleIcon.jsx
+++ src/popup/components/svg-icons/TriangleIcon.jsx
@@ -9,12 +9,17 @@ export default function TriangleIcon(props) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 220 183"
-      fill="currentColor"
+      viewBox="0 0 286 252"
+      fill="currentCOlor"
+      stroke="currentColor"
       class={`${styles.triangle} ${orientation() === "up" ? styles.up : ""} ${extra.class ?? ""}`}
       {...rest}
     >
-      <path d="M136.753 168.234C124.116 187.491 95.8844 187.491 83.2467 168.234L5.69093 50.0574C-8.27459 28.7772 6.99068 0.5 32.4442 0.5L187.556 0.5C213.009 0.5 228.275 28.7772 214.309 50.0574L136.753 168.234Z" />
+      <path
+        d="M186.478 208.19C165.942 239.49 120.058 239.49 99.5225 208.19L28.8848 100.525C6.19707 65.9447 31.0044 20 72.3633 20L213.637 20C254.996 20 279.803 65.9447 257.115 100.525L186.478 208.19Z"
+        stroke-width="30"
+        stroke-linejoin="round"
+      />
     </svg>
   );
 }
