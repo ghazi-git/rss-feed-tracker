96ce5d7fd462383de447e0aace1477cb914d6f44
ghazi <ghaziabbassi1@gmail.com>
2025-09-21 15:48:31 +0100

    create wrappers for `chrome.runtime.sendMessage` and `chrome.runtime.onMessage.addListener` to preserve types and listen to only one specific message at a time

---
+++ src/messaging-wrapper.ts
@@ -0,0 +1,43 @@
+export async function sendMessage<K extends MessageType>(
+  messageType: K,
+  args: MessagePayload<K>,
+) {
+  const response: MessageResponse<K> = await chrome.runtime.sendMessage({
+    type: messageType,
+    payload: args,
+  });
+  return response;
+}
+
+export function onMessage<K extends MessageType>(
+  messageType: K,
+  messageCallback: MessageCallback<K>,
+) {
+  chrome.runtime.onMessage.addListener(
+    (message: MessageRequest<K>, sender, sendResponse) => {
+      if (message.type === messageType) {
+        return messageCallback(message.payload, sender, sendResponse);
+      }
+    },
+  );
+}
+
+interface MessageMap {
+  // define only ONE argument for each method
+  example1(data: string): number;
+  example2(data: { test: string }): string;
+}
+
+type MessageType = keyof MessageMap;
+// assume only one argument is going to contain the payload
+type MessagePayload<K extends MessageType> = Parameters<MessageMap[K]>[0];
+type MessageRequest<K extends MessageType> = {
+  type: K;
+  payload: MessagePayload<K>;
+};
+type MessageResponse<K extends MessageType> = ReturnType<MessageMap[K]>;
+type MessageCallback<K extends MessageType> = (
+  payload: MessagePayload<K>,
+  sender: chrome.runtime.MessageSender,
+  sendResponse: (resp: MessageResponse<K>) => void,
+) => true | void;
