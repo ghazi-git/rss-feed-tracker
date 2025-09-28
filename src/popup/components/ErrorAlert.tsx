71c1a0d2a11bee2dcc9e9643173817852e8a1f47
ghazi <ghaziabbassi1@gmail.com>
2025-09-29 00:42:02 +0100

    create an error message alert

---
+++ src/popup/components/ErrorAlert.tsx
@@ -0,0 +1,11 @@
+import { Show } from "solid-js";
+
+import styles from "./ErrorAlert.module.css";
+
+export default function ErrorAlert(props: { errorMsg: string }) {
+  return (
+    <Show when={props.errorMsg}>
+      <div class={styles.alert}>{props.errorMsg}</div>
+    </Show>
+  );
+}
