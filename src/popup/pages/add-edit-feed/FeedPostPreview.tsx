d7e641c62d5eb75f1817a6c2225673d41e061b77
ghazi <ghaziabbassi1@gmail.com>
2025-09-30 23:20:02 +0100

    position the context menu properly and trigger it on right-clicking a post preview when adding a feed

--- src/popup/pages/add-edit-feed/FeedPostPreview.tsx
+++ src/popup/pages/add-edit-feed/FeedPostPreview.tsx
@@ -1,3 +1,6 @@
+import { computePosition, flip } from "@floating-ui/dom";
+
+import { usePostMenuContext } from "@/popup/components/context-menu/post-menu-context";
 import PostLink from "@/popup/components/PostLink";
 import SingleLineText from "@/popup/components/SingleLineText";
 import { formatTimestamp, humanizeTimestamp } from "@/popup/utils/datetimes";
@@ -5,8 +8,24 @@ import { formatTimestamp, humanizeTimestamp } from "@/popup/utils/datetimes";
 import styles from "./FeedPostPreview.module.css";
 
 export function FeedPostPreview(props: PostPreviewProps) {
+  const { store, showMenu } = usePostMenuContext();
   return (
-    <PostLink href={props.url} class={styles.post}>
+    <PostLink
+      href={props.url}
+      class={styles.post}
+      onContextMenu={(event) => {
+        event.preventDefault();
+        // show custom context menu instead
+        const virtualElt = getVirtualElement(event.clientX, event.clientY);
+        (async () => {
+          const { x, y } = await computePosition(virtualElt, store.menuRef!, {
+            placement: "bottom-start",
+            middleware: [flip()],
+          });
+          showMenu(props.url, y, x);
+        })();
+      }}
+    >
       <SingleLineText text={props.title} class={styles.title} />
       <div
         class={styles["published-at"]}
@@ -16,6 +35,23 @@ export function FeedPostPreview(props: PostPreviewProps) {
       </div>
     </PostLink>
   );
+}
+
+function getVirtualElement(clientX: number, clientY: number) {
+  return {
+    getBoundingClientRect() {
+      return {
+        width: 0,
+        height: 0,
+        x: clientX,
+        left: clientX,
+        right: clientX,
+        y: clientY,
+        top: clientY,
+        bottom: clientY,
+      };
+    },
+  };
 }
 
 interface PostPreviewProps {
--- src/popup/pages/add-edit-feed/FeedPostsPreview.tsx
+++ src/popup/pages/add-edit-feed/FeedPostsPreview.tsx
@@ -1,6 +1,8 @@
 import { For, Show } from "solid-js";
 
 import { PostPreview } from "@/messaging-wrapper";
+import { PostMenuProvider } from "@/popup/components/context-menu/post-menu-context";
+import { PostContextMenu } from "@/popup/components/context-menu/PostContextMenu";
 import { FeedPostPreview } from "@/popup/pages/add-edit-feed/FeedPostPreview";
 
 import styles from "./FeedPostsPreview.module.css";
@@ -11,18 +13,21 @@ export default function FeedPostsPreview(props: { posts: PostPreview[] }) {
       when={props.posts.length > 0}
       fallback={<h4 class={styles["no-posts"]}>The feed has no posts yet.</h4>}
     >
-      <div class={styles.preview}>
-        <h4 class={styles["latest-posts"]}>Latest Posts:</h4>
-        <For each={props.posts}>
-          {(post) => (
-            <FeedPostPreview
-              title={post.title}
-              url={post.url}
-              publishedAt={post.publishedAt}
-            />
-          )}
-        </For>
-      </div>
+      <PostMenuProvider>
+        <div class={styles.preview}>
+          <h4 class={styles["latest-posts"]}>Latest Posts:</h4>
+          <For each={props.posts}>
+            {(post) => (
+              <FeedPostPreview
+                title={post.title}
+                url={post.url}
+                publishedAt={post.publishedAt}
+              />
+            )}
+          </For>
+        </div>
+        <PostContextMenu />
+      </PostMenuProvider>
     </Show>
   );
 }
