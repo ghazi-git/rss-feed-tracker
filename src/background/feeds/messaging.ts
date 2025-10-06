aad87e4dedae2e6618eed179abf90ac0bb169af8
ghazi <ghaziabbassi1@gmail.com>
2025-10-06 22:42:43 +0100

    wrap feed preview logic in a single function to be able to catch any errors thrown

--- src/background/feeds/messaging.ts
+++ src/background/feeds/messaging.ts
@@ -1,18 +1,9 @@
-import { fetchFeedContent, parseFeedContent } from "@/background/feeds/fetch";
+import { previewFeed } from "@/background/feeds/preview";
 import { onMessage } from "@/messaging-wrapper";
 
 onMessage("feeds/preview", (payload, sender, sendResponse) => {
-  fetchFeedContent(payload.url)
-    .then((feedContent) => {
-      const feed = parseFeedContent(payload.url, feedContent);
-      const data = {
-        feedName: feed.name,
-        posts: feed.posts.slice(0, 3).map(({ title, url, publishedAt }) => ({
-          title,
-          url,
-          publishedAt,
-        })),
-      };
+  previewFeed(payload.url)
+    .then((data) => {
       sendResponse({ success: true, data, errorMsg: null });
     })
     .catch((reason: Error) => {
---
+++ src/background/feeds/preview.ts
@@ -0,0 +1,18 @@
+import { fetchFeedContent, parseFeedContent } from "@/background/feeds/fetch";
+import { FeedPreviewResponse } from "@/messaging-wrapper";
+
+/**
+ * get the latest 3 posts from the provided feed URL
+ */
+export async function previewFeed(url: string): Promise<FeedPreviewResponse> {
+  const feedContent = await fetchFeedContent(url);
+  const feed = parseFeedContent(url, feedContent);
+  return {
+    feedName: feed.name,
+    posts: feed.posts.slice(0, 3).map(({ title, url, publishedAt }) => ({
+      title,
+      url,
+      publishedAt,
+    })),
+  };
+}
