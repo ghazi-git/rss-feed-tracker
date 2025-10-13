f23915d173d75e1b1e93caba7e1bfb98991076ae
ghazi <ghaziabbassi1@gmail.com>
2025-10-13 11:12:06 +0100

    set the UnstyledButton type attribute to button to avoid defaulting to submit

--- src/popup/components/buttons/UnstyledButton.tsx
+++ src/popup/components/buttons/UnstyledButton.tsx
@@ -5,7 +5,11 @@ import styles from "./UnstyledButton.module.css";
 export default function UnstyledButton(props: ButtonProps) {
   const [extra, btnProps] = splitProps(props, ["class"]);
   return (
-    <button class={`${styles.unstyled} ${extra.class ?? ""}`} {...btnProps} />
+    <button
+      type="button"
+      class={`${styles.unstyled} ${extra.class ?? ""}`}
+      {...btnProps}
+    />
   );
 }
 
