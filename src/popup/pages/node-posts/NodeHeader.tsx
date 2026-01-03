573fd4d99991380629de11c8aa11b2b8c9935c0a
ghazi <ghaziabbassi1@gmail.com>
2026-01-03 21:33:32 +0100

    display the node header in the node posts page

---
+++ src/popup/pages/node-posts/NodeHeader.tsx
@@ -0,0 +1,46 @@
+import { useSearchParams } from "@solidjs/router";
+import { Show } from "solid-js";
+
+import { TreeNode } from "@/background/db-setup";
+import BackLink from "@/popup/components/page-header/BackLink";
+import PageHeaderWrapper from "@/popup/components/page-header/PageHeaderWrapper";
+import PageTitleButton from "@/popup/components/page-header/PageTitleButton";
+import PostsFilter from "@/popup/pages/node/PostsFilter";
+
+import styles from "./NodeHeader.module.css";
+
+export default function NodeHeader(props: NodeHeaderProps) {
+  const [searchParams] = useSearchParams();
+  const previousUrl = () => {
+    if (props.node.type === "folder") {
+      return `/library/nodes/${props.node.id}`;
+    } else {
+      return `/library/nodes/${props.node.parentId}`;
+    }
+  };
+
+  return (
+    <PageHeaderWrapper>
+      <BackLink url={previousUrl()} class={styles["previous-url"]} />
+      <PageTitleButton
+        title={props.node.name}
+        nodeType={props.node.type}
+        nodeId={props.node.id}
+        nodeName={props.node.name}
+        isRoot={props.node.parentId === null}
+      />
+      <Show when={props.node.hasPosts}>
+        <PostsFilter
+          unreadCount={props.node.unreadCount}
+          pageUrl={`/library/nodes/${props.node.id}/posts`}
+          initialFilter={searchParams.unread === "true" ? "unread" : "all"}
+          class={styles["posts-filter"]}
+        />
+      </Show>
+    </PageHeaderWrapper>
+  );
+}
+
+interface NodeHeaderProps {
+  node: TreeNode & { hasPosts: boolean };
+}
--- src/popup/pages/node-posts/index.tsx
+++ src/popup/pages/node-posts/index.tsx
@@ -1,67 +1,18 @@
 import { useParams, useSearchParams } from "@solidjs/router";
-import { createMemo, Show } from "solid-js";
+import { createResource, Match, Show, Switch } from "solid-js";
 
-import { Post, TreeNode } from "@/background/db-setup";
+import { sendMessage } from "@/messaging-wrapper";
 import { DeleteNodeProvider } from "@/popup/components/delete-node-dialog/context";
 import DeleteNodeDialog from "@/popup/components/delete-node-dialog/DeleteNodeDialog";
 import NoPosts from "@/popup/components/NoPosts";
-import BackLink from "@/popup/components/page-header/BackLink";
-import PageHeaderWrapper from "@/popup/components/page-header/PageHeaderWrapper";
-import PageTitleButton from "@/popup/components/page-header/PageTitleButton";
-import PostsFilter from "@/popup/pages/node/PostsFilter";
+import NodeHeader from "@/popup/pages/node-posts/NodeHeader";
 import Posts from "@/popup/pages/node-posts/Posts";
 
 import styles from "./index.module.css";
 
 export default function NodePosts() {
+  const { node } = createNodeResource();
   const [searchParams] = useSearchParams();
-  const params = useParams();
-  const node = () =>
-    ([] as TreeNode[]).find((n) => n.id === parseInt(params.id));
-  const allPosts = createMemo(() => {
-    const currentNode = node();
-    if (!currentNode) return [];
-
-    let posts;
-    if (currentNode.type === "folder") {
-      const feedIds: number[] = [];
-      const folderIds = [currentNode.id];
-      while (folderIds.length > 0) {
-        const folderId = folderIds.shift();
-        const children = ([] as TreeNode[]).filter(
-          (n) => n.parentId === folderId,
-        );
-        for (const child of children) {
-          if (child.type === "folder") folderIds.push(child.id);
-          else feedIds.push(child.id);
-        }
-      }
-      posts = ([] as Post[]).filter((post) => feedIds.includes(post.feedId));
-      posts = posts.map((post) => {
-        const n = ([] as TreeNode[]).find((nd) => nd.id === post.feedId);
-        return {
-          ...post,
-          feed: { name: n!.name, favicon: n!.feed!.favicon },
-        };
-      });
-    } else {
-      posts = ([] as Post[]).filter((post) => post.feedId === currentNode.id);
-      posts = posts.map((post) => ({
-        ...post,
-        feed: { name: currentNode.name, favicon: currentNode.feed.favicon },
-      }));
-    }
-
-    posts.sort((a, b) => b.publishedAt - a.publishedAt);
-    return posts;
-  });
-  const filteredPosts = createMemo(() => {
-    if (searchParams.unread === "true") {
-      return allPosts().filter((post) => post.unread);
-    } else {
-      return allPosts();
-    }
-  });
   const noPostsMsg = () => {
     if (searchParams.unread === "true") {
       return "No unread posts";
@@ -69,50 +20,43 @@ export default function NodePosts() {
       return "No posts published yet";
     }
   };
-  const previousUrl = () => {
-    const currentNode = node();
-    if (!currentNode) return "/library";
-
-    if (currentNode.type === "folder") {
-      return `/library/nodes/${currentNode.id}`;
-    } else {
-      return `/library/nodes/${currentNode.parentId}`;
-    }
-  };
 
   return (
-    <Show when={node()} fallback={<h2>Feed/Folder not Found</h2>}>
-      {(currentNode) => (
-        <DeleteNodeProvider>
-          <PageHeaderWrapper>
-            <BackLink url={previousUrl()} class={styles["previous-url"]} />
-            <PageTitleButton
-              title={currentNode().name}
-              nodeType={currentNode().type}
-              nodeId={currentNode().id}
-              nodeName={currentNode().name}
-              isRoot={currentNode().parentId === null}
-            />
-            <Show when={allPosts().length > 0}>
-              <PostsFilter
-                unreadCount={currentNode().unreadCount}
-                pageUrl={`/library/nodes/${currentNode().id}/posts`}
-                initialFilter={
-                  searchParams.unread === "true" ? "unread" : "all"
-                }
-                class={styles["posts-filter"]}
-              />
-            </Show>
-          </PageHeaderWrapper>
-          <Show
-            when={filteredPosts().length > 0}
-            fallback={<NoPosts msg={noPostsMsg()} />}
-          >
-            <Posts posts={filteredPosts()} />
-          </Show>
-          <DeleteNodeDialog />
-        </DeleteNodeProvider>
-      )}
-    </Show>
+    <DeleteNodeProvider>
+      <Switch>
+        <Match when={node.error}>
+          <div class={styles.error}>
+            {node.error.message || "An unexpected error occurred"}
+          </div>
+        </Match>
+        <Match when={node.loading}>
+          <div class={styles["blank-space-while-loading-header"]} />
+        </Match>
+        <Match when={node()}>
+          {(currentNode) => <NodeHeader node={currentNode()} />}
+        </Match>
+      </Switch>
+
+      <Show when={[].length > 0} fallback={<NoPosts msg={noPostsMsg()} />}>
+        <Posts posts={[]} />
+      </Show>
+      <DeleteNodeDialog />
+    </DeleteNodeProvider>
   );
+}
+
+function createNodeResource() {
+  const params = useParams();
+  const [node, { mutate }] = createResource(
+    () => parseInt(params.id),
+    async (id) => {
+      const response = await sendMessage("nodes/get-for-node-posts-page", {
+        id,
+      });
+      if (!response.success) throw new Error(response.errorMsg);
+
+      return response.data;
+    },
+  );
+  return { node, mutateNode: mutate };
 }
