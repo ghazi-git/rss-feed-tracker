c3b8b5d07fa8d51dd6eaf479fb21f20004324143
ghazi <ghaziabbassi1@gmail.com>
2026-01-23 09:24:03 +0100

    restore the last visited URL when opening the popup only within 15 mins of when it was last visited

--- src/popup/utils/last-visited-page.ts
+++ src/popup/utils/last-visited-page.ts
@@ -1,7 +1,22 @@
 export function saveLastVisitedPage(url: string) {
-  window.localStorage.setItem("lastVisitedURL", url);
+  const data = { url, savedAt: new Date().toISOString() };
+  window.localStorage.setItem("lastVisitedURL", JSON.stringify(data));
 }
 
+/**
+ * return the last visited URL only if was saved less than 15 mins ago. This is
+ * helpful when users mistakenly click outside the popup causing it to close
+ */
 export function getLastVisitedPage(): string | null {
-  return window.localStorage.getItem("lastVisitedURL");
+  const data = window.localStorage.getItem("lastVisitedURL");
+  if (!data) return null;
+
+  try {
+    const { url, savedAt } = JSON.parse(data);
+    const dt = new Date(savedAt);
+    if (typeof url === "string" && dt.getTime() > Date.now() - 15 * 60 * 1000) {
+      return url;
+    }
+  } catch {}
+  return null;
 }
