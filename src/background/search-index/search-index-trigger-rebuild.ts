d1b4316ac4146b6b3204587b03b0e03c2a897de1
ghazi <ghaziabbassi1@gmail.com>
2026-02-18 14:34:42 +0100

    update trigger search index rebuild message handler to check that there are no other processes doing the rebuilding and that there are posts to index first

--- src/background/search-index/search-index-trigger-rebuild.ts
+++ src/background/search-index/search-index-trigger-rebuild.ts
@@ -1,12 +1,23 @@
+import { isRebuildingSearchIndex } from "@/background/search-index/search-index-is-rebuild-in-progress";
 import { TriggerRebuildSearchIndex } from "@/background/utils/errors";
 import { setupOffscreenDocument } from "@/background/utils/offscreen";
+import { getDBConnection } from "@/db-setup";
 import { sendMessage } from "@/messaging-wrapper";
 
 export async function triggerRebuildSearchIndex() {
-  await setupOffscreenDocument("Build a search index");
+  using conn = await getDBConnection();
+  const posts = await conn.db.getAll("posts", { count: 1 });
+  if (!posts.length)
+    throw new TriggerRebuildSearchIndex("There are no posts to index.");
 
-  const response = await sendMessage("search-index/rebuild", undefined);
-  if (!response.success) {
-    throw new TriggerRebuildSearchIndex(response.errorMsg);
-  }
+  const inProgress = await isRebuildingSearchIndex();
+  if (inProgress)
+    throw new TriggerRebuildSearchIndex(
+      "Search index rebuilding is already in progress. Please wait until it finishes.",
+    );
+
+  await setupOffscreenDocument("Build a search index");
+  // trigger the indexing but don't wait for it to finish as it can take up to
+  // 90 minutes for 90K posts
+  sendMessage("search-index/rebuild", undefined);
 }
