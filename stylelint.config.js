dca64dfc5e9e584c6f5a89e76fb14c34c44647a7
ghazi <ghaziabbassi1@gmail.com>
2026-02-26 19:55:00 +0100

    update stylelint

--- stylelint.config.js
+++ stylelint.config.js
@@ -3,7 +3,8 @@ export default {
   extends: ["stylelint-config-standard"],
   plugins: ["stylelint-plugin-logical-css"],
   rules: {
-    "plugin/use-logical-properties-and-values": [true, { severity: "warning" }],
-    "plugin/use-logical-units": [true, { severity: "warning" }],
+    "logical-css/require-logical-keywords": [true, { severity: "warning" }],
+    "logical-css/require-logical-properties": [true, { severity: "warning" }],
+    "logical-css/require-logical-units": [true, { severity: "warning" }],
   },
 };
