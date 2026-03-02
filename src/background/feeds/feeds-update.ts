b091772b83dbdf8c09ca93fbe000ea8fd63e918c
ghazi <ghaziabbassi1@gmail.com>
2026-03-02 22:51:01 +0100

    update feeds-update flow to account for feedmetadata removal

--- src/background/feeds/feeds-update.ts
+++ src/background/feeds/feeds-update.ts
@@ -1,5 +1,4 @@
 import { NotFoundError } from "@/background/utils/errors";
-import { getInitialFeedmetadata } from "@/background/utils/feedmetadata";
 import {
   getAncestors,
   getHighestSortOrder,
@@ -13,12 +12,12 @@ import { txDone } from "@/utils/idb-helpers";
  * - Save the updated feed data provided by the user
  * - Update the sortOrder and unread counts of old and new parent folders
  * if the parent folder changes
- * - Update feed metadata if the update frequency changes
+ * - Update feed run times if the update frequency changes
  */
 export async function updateFeed(id: number, feedData: UpdateFeedFormData) {
   using conn = await getDBConnection();
 
-  const tx = conn.db.transaction(["nodes", "feedmetadata"], "readwrite");
+  const tx = conn.db.transaction(["nodes"], "readwrite");
   const nodeStore = tx.objectStore("nodes");
   const old = await nodeStore.get(id);
   if (!old || old.type !== "feed") {
@@ -37,46 +36,38 @@ export async function updateFeed(id: number, feedData: UpdateFeedFormData) {
     updated.sortOrder = await getHighestSortOrder(tx, newParentId);
   }
   updated.feed.url = feedData.url;
-  updated.feed.updateFrequency = feedData.frequency;
   updated.feed.favicon = feedData.iconURL || null;
-  await nodeStore.put(updated);
-
-  if (newParentId && newParentId !== old.parentId) {
-    // update the unread counts
-    if (updated.unreadCount) {
-      const nodes = await nodeStore.getAll();
-      const nodeMap = getNodeMap(nodes);
-      // update the unread count of the previous ancestors
-      const oldAncestors = getAncestors(old.parentId, nodeMap);
-      const oldPromises = oldAncestors.map((a) => {
-        a.unreadCount = Math.max(a.unreadCount - updated.unreadCount, 0);
-        return nodeStore.put(a);
-      });
-      await Promise.all(oldPromises);
-      // update the unread count of the new ancestors
-      const newAncestors = getAncestors(newParentId, nodeMap);
-      const newPromises = newAncestors.map((a) => {
-        a.unreadCount = a.unreadCount + updated.unreadCount;
-        return nodeStore.put(a);
-      });
-      await Promise.all(newPromises);
-    }
-  }
-
+  updated.feed.updateFrequency = feedData.frequency;
   if (updated.feed.updateFrequency !== old.feed.updateFrequency) {
-    const metadataStore = tx.objectStore("feedmetadata");
-    let metadata = await metadataStore.get(id);
-    if (!metadata) {
-      metadata = getInitialFeedmetadata(id);
-    }
     let nextRunAt: number | null = null;
     if (updated.feed.updateFrequency) {
-      nextRunAt = metadata.lastRunAt
-        ? metadata.lastRunAt + updated.feed.updateFrequency
+      nextRunAt = updated.feed.lastRunAt
+        ? updated.feed.lastRunAt + updated.feed.updateFrequency
         : Date.now();
     }
-    metadata.nextRunAt = nextRunAt;
-    await metadataStore.put(metadata);
+    updated.feed.nextRunAt = nextRunAt;
   }
+  await nodeStore.put(updated);
+
+  if (newParentId && newParentId !== old.parentId && updated.unreadCount) {
+    // update the unread counts
+    const nodes = await nodeStore.getAll();
+    const nodeMap = getNodeMap(nodes);
+    // update the unread count of the previous ancestors
+    const oldAncestors = getAncestors(old.parentId, nodeMap);
+    const oldPromises = oldAncestors.map((a) => {
+      a.unreadCount = Math.max(a.unreadCount - updated.unreadCount, 0);
+      return nodeStore.put(a);
+    });
+    await Promise.all(oldPromises);
+    // update the unread count of the new ancestors
+    const newAncestors = getAncestors(newParentId, nodeMap);
+    const newPromises = newAncestors.map((a) => {
+      a.unreadCount = a.unreadCount + updated.unreadCount;
+      return nodeStore.put(a);
+    });
+    await Promise.all(newPromises);
+  }
+
   await txDone(tx);
 }
