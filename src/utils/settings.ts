6315ef128bacda42c4a6b3dee15a375ee5cf4e8c
ghazi <ghaziabbassi1@gmail.com>
2026-03-09 21:43:21 +0100

    set SEARCH_RESULTS_LIMIT the same as PAGE_SIZE

--- src/utils/settings.ts
+++ src/utils/settings.ts
@@ -2,7 +2,10 @@ export const PAGE_SIZE = 50;
 export const ICONS_CACHE = "image-cache-v1";
 export const SORT_ORDER_STEP = 10_000;
 export const RECENT_POSTS_LIMIT = 1000;
-export const SEARCH_RESULTS_LIMIT = 100;
+// set the SEARCH_RESULTS_LIMIT the same as PAGE_SIZE. That way, when moving
+// from the folder posts page to the filtering page while "group posts by feed"
+// is enabled, the posts displayed remain the same in the same order
+export const SEARCH_RESULTS_LIMIT = PAGE_SIZE;
 export const SEARCH_INDEX_DEFAULT_STORE = "search-index";
 export const SEARCH_INDEX_REBUILDING_LOCK = "search-index-rebuilding";
 export const SEARCH_INDEX_REBUILDING_ALARM = "search-index-rebuilding";
